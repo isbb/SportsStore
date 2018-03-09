c5762cd1e169c16c8693899b0695a1169ddb7a39
richard wang <wang.richard@tycoelectronics.com>
2018-03-09 16:35:25 +0800

    one day

---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyReslver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyReslver:IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyReslver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+
+        }
+    }
+}
